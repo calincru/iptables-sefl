7e70263dea864b1887a139fbf6ddaaedce6186a0
Calin Cruceru <calin@rosedu.org>
2017-05-29 01:44:17 +0300

    Finish implementing the script for translating iptables rules from 'show' format to the more general format expected by our tool

--- scripts/iptables_from_show_to_mine.py
+++ scripts/iptables_from_show_to_mine.py
@@ -11,8 +11,38 @@ def main(args):
 
     tables = {}
     for i, table_file in enumerate(table_files):
-        # TODO: Implement this.
-        pass
+        curr_table = {}
+
+        with open(table_file, mode='r') as f:
+            for line in f:
+                tokens = line.split()
+                if tokens[0] == '-P':
+                    curr_table[tokens[1]] = {
+                            'default_policy': tokens[2],
+                            'rules': []
+                    }
+                elif tokens[0] == '-N':
+                    curr_table[tokens[1]] = {
+                            'rules': []
+                    }
+                elif tokens[0] == '-A':
+                    curr_table[tokens[1]]['rules'].append(' '.join(tokens[2:]))
+                else:
+                    raise Exception('Unknown iptables command!')
+        tables[TABLE_NAMES[i]] = curr_table
+
+    with open('iptables.out', mode='w') as f:
+        for table_name, chains in tables.items():
+            print('<<{}>>'.format(table_name), file=f)
+            for chain_name, chain_data in chains.items():
+                if 'default_policy' in chain_data:
+                    print('<{}:{}>'.format(chain_name,
+                                           chain_data['default_policy']),
+                          file=f)
+                else:
+                    print('<{}>'.format(chain_name), file=f)
+                for rule in chain_data['rules']:
+                    print(rule, file=f)
 
 
 if __name__ == '__main__':
